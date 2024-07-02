e99819238031a4af587d0fee8ab1745dd87e8c2e
finn <finn@interparktriple.com>
2024-07-02 15:08:18 +0900

    채팅룸 페이지 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,5 @@
-import React from 'react'
+import { ChatRoom } from '@/chat-room'
 
-import Home, { HomeProps } from '@/home'
-
-/**
- * FIXME: 예시 코드로 초기 프로젝트 설정 후 삭제하세요.
- */
-export default function HomePage({ todos }: HomeProps) {
-  return <Home todos={todos} />
-}
-
-HomePage.getInitialProps = async function () {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos')
-  const todos = (await res.json()) as unknown[]
-
-  return { todos: todos.slice(0, 5) }
+export default function ChatRoomPage() {
+  return <ChatRoom />
 }
---
+++ src/chat-room/chat-room.tsx
@@ -0,0 +1,3 @@
+export function ChatRoom() {
+  return <div>test</div>
+}
